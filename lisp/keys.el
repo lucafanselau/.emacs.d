2c24027e1dbcd2c79dfad3e726231e22fae15094
Luca Fanselau <luca.fanselau@outlook.com>
2023-04-10 18:17:49 +0200

    Disable puni again

--- lisp/keys.el
+++ lisp/keys.el
@@ -90,14 +90,14 @@
  (require 'meow) (meow-setup) (meow-global-mode 1))
 
 ;; Use puni-mode globally and disable it for term-mode.
-(use-package
- puni
- :defer t
- :init
- ;; The autoloads of Puni are set up so you can enable `puni-mode` or
- ;; `puni-global-mode` before `puni` is actually loaded. Only after you press
- ;; any key that calls Puni commands, it's loaded.
- (puni-global-mode)
- (add-hook 'term-mode-hook #'puni-disable-puni-mode))
+;; (use-package
+;;  puni
+;;  :defer t
+;;  :init
+;;  ;; The autoloads of Puni are set up so you can enable `puni-mode` or
+;;  ;; `puni-global-mode` before `puni` is actually loaded. Only after you press
+;;  ;; any key that calls Puni commands, it's loaded.
+;;  (puni-global-mode)
+;;  (add-hook 'term-mode-hook #'puni-disable-puni-mode))
 
 (provide 'keys)
